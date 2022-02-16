39398fd05b506c83f10b3730b8966115b211c7b8
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-02-16 13:38:36 -0500

    create

---
+++ Barcode/main.js
@@ -0,0 +1,38 @@
+if (!('BarcodeDetector' in window)) { 
+    alert('Barcode Error Browser not Compatible')
+}
+
+var videofeed = document.getElementById('video')
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
+  navigator.mediaDevices.getUserMedia({video:true, audio:false, facingMode:'user'})
+  .then(function(stream){ 
+    console.log(navigator.mediaDevices)
+      videofeed.srcObject = stream});
+}
+
+var barcodeDetector = new BarcodeDetector({formats:['upc_a']})
+var list = []
+
+function render() {
+    barcodeDetector.detect(video).then((barcodes) => {
+        barcodes.forEach((barcode) => {
+        
+            list.push(barcode.rawValue)
+            console.log(list)
+            console.log(barcode.rawValue);
+            playBeep()
+
+        });
+      })
+  }
+
+  setInterval(function() {
+  render();
+}, 2000);
+
+var audioSorce = document.getElementById("barcodeBeep")
+function playBeep(){
+audioSorce.play()
+}
