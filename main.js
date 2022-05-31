feeb69aa310c06f5bb609925492cfa7765cf8cc8
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-05-31 11:01:13 -0400

    video

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 
-if (!('BarcodeDetector' in window)) { 
-    alert('Barcode Error Browser not Compatible')
-}
+// if (!('BarcodeDetector' in window)) { 
+//     alert('Barcode Error Browser not Compatible')
+// }
 
-var videofeed = document.getElementById('video')
+// var videofeed = document.getElementById('video')
 
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+// if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
-  navigator.mediaDevices.getUserMedia({video:{facingMode:{
-    exact: 'environment'
-  }}, audio:false})
-  .then(function(stream){ 
-    console.log(navigator.mediaDevices)
-      videofeed.srcObject = stream});
-}
+//   navigator.mediaDevices.getUserMedia({video:{facingMode:{
+//     exact: 'environment'
+//   }}, audio:false})
+//   .then(function(stream){ 
+//     console.log(navigator.mediaDevices)
+//       videofeed.srcObject = stream});
+// }
 
-var barcodeDetector = new BarcodeDetector({formats:['upc_a']})
-var list = []
+// var barcodeDetector = new BarcodeDetector({formats:['upc_a']})
+// var list = []
 
-function render() {
-    barcodeDetector.detect(video).then((barcodes) => {
-        barcodes.forEach((barcode) => {
+// function render() {
+//     barcodeDetector.detect(video).then((barcodes) => {
+//         barcodes.forEach((barcode) => {
         
-            list.push(barcode.rawValue)
-            console.log(list)
-            console.log(barcode.rawValue);
-            playBeep()
+//             list.push(barcode.rawValue)
+//             console.log(list)
+//             console.log(barcode.rawValue);
+//             playBeep()
 
-        });
-      })
-  }
+//         });
+//       })
+//   }
 
-  setInterval(function() {
-  render();
-}, 2000);
+//   setInterval(function() {
+//   render();
+// }, 2000);
 
-var audioSorce = document.getElementById("barcodeBeep")
-function playBeep(){
-audioSorce.play()
-}
+// var audioSorce = document.getElementById("barcodeBeep")
+// function playBeep(){
+// audioSorce.play()
+// }
